3994fcd433e88ae5e7e0d093abbc76118d7c2b66
parabirb <parabirb@protonmail.ch>
2024-04-11 13:27:23 -0500

    start work. can't figure out how to get grub-mkrescue on here so i'll do that when i'm home

---
+++ build.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) !void {
+    var target: std.zig.CrossTarget = .{
+        .cpu_arch = .x86,
+        .os_tag = .freestanding,
+        .abi = .none,
+    };
+
+    const optimize = b.standardOptimizeOption(.{});
+    const kernel = b.addExecutable(.{
+        .name = "kernel",
+        .root_source_file = .{ .path = "kernel/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    kernel.code_model = .kernel;
+    kernel.setLinkerScriptPath(.{ .path = "kernel/linker.ld" });
+    kernel.pie = true;
+
+    b.installArtifact(kernel);
+}
