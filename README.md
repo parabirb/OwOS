ab6fd9f65775e83a63e843c0e20d597ebeaeb2da
birb <parabirb@protonmail.ch>
2024-04-12 00:39:26 -0500

    bark

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # OwOS
+OwOS is a simple 32-bit kernel written in Zig as practice.
--- kernel/main.zig
+++ kernel/main.zig
@@ -3,7 +3,6 @@ const cmd = @import("console/cmd.zig");
 export fn _start() callconv(.C) noreturn {
     cmd.putString("hello, OwOS!", 0xf, 0x0);
     for (1..26) |i| cmd.putString("\nhello, OwOS!", 0xf, @truncate(i % 4));
-    while (true) {
-        asm volatile ("hlt");
-    }
+    asm volatile ("cli");
+    asm volatile ("hlt");
 }
