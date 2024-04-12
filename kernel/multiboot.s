44c6537960752af60548315fff02eba244a5fc2d
birb <parabirb@protonmail.ch>
2024-04-11 21:47:48 -0500

    do some linker fuckery to get this shit to work i fucking hate you zig

--- kernel/multiboot.s
+++ kernel/multiboot.s
@@ -1,12 +1,8 @@
-/*
-    stolen from xinos, used chatgpt to convert to at&t
-    because i'm lazy like that
-*/
-.equ FLAGS, (1 << 0) | (1 << 1)
+.equ FLAGS, 0
 .equ MAGIC, 0x1badb002
 .equ CHECKSUM, -(MAGIC + FLAGS)
 
-.section .multiboot, "a"
+.section .multiboot
     .align 4
     .long MAGIC
     .long FLAGS
