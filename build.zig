fa8885e7920037429cfa4a8252e25b3ebf00b558
birb <parabirb@protonmail.ch>
2025-05-17 15:15:00 -0500

    zig 0.14.0

--- build.zig
+++ build.zig
@@ -10,12 +10,12 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const kernel = b.addExecutable(.{
         .name = "kernel",
-        .root_source_file = .{ .path = "kernel/main.zig" },
+        .root_source_file = b.path("kernel/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    kernel.addAssemblyFile(.{ .path = "kernel/boot/multiboot.s" });
-    kernel.setLinkerScriptPath(.{ .path = "kernel/linker.ld" });
+    kernel.addAssemblyFile(b.path("kernel/boot/multiboot.s"));
+    kernel.setLinkerScript(b.path("kernel/linker.ld"));
     kernel.pie = true;
 
     b.installArtifact(kernel);
